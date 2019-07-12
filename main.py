bb6a21b4eab79aa66afb1fe48c863e53b13fcc3c
Joshua Fung <jfung@westhillinnvation.com>
2019-07-12 12:55:17 -0400

    changed for TFTFeatherWing

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 import lvgl as lv
-from SI7021 import SI7021
-lv.init()
 import utime
-# Import ESP32 driver 
 
-import lvesp32
+#Import TFTFeatherWing, initialize it and register it with LittlevGL
+import TFTFeatherWing as tftWing
 
-#Import ILI9341, initialize it and register it with LittlevGL
+lv.init()
 
-import HX8357 as hx
+tft = tftWing.TFTFeatherWing()
+tft.init()
 
-th_sensor = SI7021(22,23)
-
-d = hx.display(mhz=24, miso=19, mosi=18, clk=5, cs=15, dc=33, rst=4, backlight=-1)
-d.init()
 disp_buf1 = lv.disp_buf_t()
 buf1_1 = bytes(480*10)
 lv.disp_buf_init(disp_buf1,buf1_1, None, len(buf1_1)//4)
 disp_drv = lv.disp_drv_t()
 lv.disp_drv_init(disp_drv)
+
 disp_drv.buffer = disp_buf1
-disp_drv.flush_cb = d.flush
+disp_drv.flush_cb = tft.flush
 disp_drv.hor_res = 480
 disp_drv.ver_res = 320
 lv.disp_drv_register(disp_drv)
@@ -33,7 +29,7 @@ header.set_width(480)
 sym = lv.label(header)
 sym.set_text(lv.SYMBOL.WIFI + " " + str(utime.localtime()))
 header_text = lv.label(header)
-header_text.set_text("T: " + str(th_sensor.read_temperature) + " R: " + str(th_sensor.read_relative_humidity))
+#header_text.set_text("T: " + str(th_sensor.read_temperature) + " R: " + str(th_sensor.read_relative_humidity))
 
 header_text.align(header, lv.ALIGN.IN_LEFT_MID, 10, 0)
 
@@ -46,7 +42,7 @@ header.set_pos(0, 0)
 lv.scr_load(scr)
 
 while True:
-    header_text.set_text("T: " + str(th_sensor.read_temperature) + " R: " + str(th_sensor.read_relative_humidity))
+    #header_text.set_text("T: " + str(th_sensor.read_temperature) + " R: " + str(th_sensor.read_relative_humidity))
     sym.set_text(lv.SYMBOL.WIFI + " " + str(utime.localtime()))
     utime.sleep(0.5)
 
