59f102d509b758cf0cdf21a25505c8a67ab4ec93
Joshua Fung <jfung@westhillinnvation.com>
2019-07-18 17:26:09 -0400

    test for python module

--- init_test/pkg1/module1.py
+++ init_test/pkg1/module1.py
@@ -1,11 +1,11 @@
 # pkg1/module1.py
-import .
+import pkg1
 
 class Module1:
     def __init__(self):
         self.name = "Module 1"
         print(dir())
-        print(const_init)
+        print(pkg1.const_init)
         print(const_a)
 
     def __str__(self):
