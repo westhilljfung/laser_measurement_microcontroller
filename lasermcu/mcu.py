1e1575b537d9308793bb3977c4a42cf1525ebb7f
Joshua Fung <jfung@westhillinnvation.com>
2019-07-18 19:01:20 -0400

    dt[3] can't be just -4

--- lasermcu/mcu.py
+++ lasermcu/mcu.py
@@ -33,8 +33,8 @@ class LaserMCU:
     def get_local_time_str(self):
         # TODO Daylight Saving Time
         dt = utime.locatime()
-        dt_str = str(dt[0]) + "-" + str(dt[1]) + "-" str(dt[2]) + " " \
-            + str(dt[3]) + ":" str(dt[4]) + " " + str(dt[5])
+        dt_str = str(dt[0]) + "-" + str(dt[1]) + "-" + str(dt[2]) + " " \
+            + str(dt[3] - 4) + ":" str(dt[4]) + " " + str(dt[5])
         return dt_str
     
     def is_connected(self):
