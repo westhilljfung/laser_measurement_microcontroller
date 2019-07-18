80ee59d89e9c613bfef11a7308f1ee5a4c5f4e16
Joshua Fung <jfung@westhillinnvation.com>
2019-07-18 15:17:03 -0400

    try to create a base System module

---
+++ sys_status/Sys.py
@@ -0,0 +1,31 @@
+import network
+import ntptime
+import utime
+
+class Sys:
+
+    def __init__(self):
+        name = "Westhill Laser Measument System"
+        wlan =  network.WLAN(network.STA_IF)
+        self.connect_wifi()
+        self.set_time()
+
+    def connect_wifi(self):
+        if not self.is_connected():
+            self.wlan.active(True)
+            self.connect(ssid,wp2_pass)            
+        return
+        
+    def set_time(self):
+        if self.is_connected():
+            ntptime.settime()
+        else:
+            raise RuntimeError("Wifi is diconnected")
+        return
+    
+    def is_connected(self):
+        return self.wlan.isconnected()
+
+    def __str__(self):
+        return name
+        
---
+++ sys_status/__init__.py
@@ -0,0 +1,2 @@
+ssid = 'Westhill_2.4G'
+wp2_pass = 'Radoslav13'
