c3356f0e19f31923181ab3ff7ba9060fbc3a0ddb
Joshua Fung <jfung@westhillinnvation.com>
2019-07-19 13:48:07 -0400

    th_sensor

--- gui_ctrl.py
+++ gui_ctrl.py
@@ -69,7 +69,7 @@ class LaserGui:
     
         self._sym.set_text(self._laser_mcu.get_local_time_str() + " " + lv.SYMBOL.WIFI)
         self._header_text = lv.label(self._header)
-        self._header_text.set_text("T: " + self._th_sensors.read_temperature())
+        self._header_text.set_text("T: " + self._th_sensor.read_temperature())
         self._header_text.align(self._header, lv.ALIGN.IN_LEFT_MID, 10, 0)
 
         self._sym.align(self._header, lv.ALIGN.IN_RIGHT_MID, -10, 0)
