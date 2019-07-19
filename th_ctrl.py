26d7b54d01f7f822b801d98fd4eb915f5bf708a5
Joshua Fung <jfung@westhillinnvation.com>
2019-07-19 15:01:11 -0400

    added th_ctrl

--- th_ctrl.py
+++ th_ctrl.py
@@ -1,2 +1,10 @@
 # th_ctrl.py
 import si7021
+
+class THCtrl:
+    def __init__(self):
+        self._th_sensor = si7021.SI7021(22, 23)
+
+    def get_th_str(self):
+        str = "T: " + str("%0.2f" % self._th_sensor.read_temperature()) + " H: " \
+            + str("%0.2f" % self._th_sensor.read_relative_humidity())
