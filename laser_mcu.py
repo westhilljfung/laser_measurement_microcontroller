a35d9368ed2192b0a78747be7a65bc8d334b63f1
Joshua Fung <jfung@westhillinnvation.com>
2019-07-20 02:49:26 -0400

    try to get it to work even without wifi

--- laser_mcu.py
+++ laser_mcu.py
@@ -8,6 +8,7 @@ ssid = 'Westhill_2.4G'
 wp2_pass = 'Radoslav13'
 
 TIME_ZONE_OFFSET = const(14400)
+WIFI_CON_TIMEOUT = const(60000)
 
 class LaserMCU:
 
@@ -15,31 +16,32 @@ class LaserMCU:
         self.name = "Westhill Laser Measument System"
         self.wlan =  network.WLAN(network.STA_IF)
         self.connect_wifi()
-        self.set_time()
-        self.time_created = self.get_time()
 
     def connect_wifi(self):
         if not self.is_connected():
             self.wlan.active(True)
             self.wlan.connect(ssid, wp2_pass)
-            while not self.is_connected():
-                utime.sleep(1)
+            start = utime.ticks_ms()
+            break_loop = False
+            while not break_loop:
+                utime.sleep_ms(5)
+                if utime.ticks_diff(utime.ticks_ms, start) >= WIFI_CONTIMEOUT\
+                   or self.is_connected():
+                    break_loop = True
         return
         
     def set_time(self):
-        if self.is_connected():
-            ntptime.settime()
-        else:
-            raise RuntimeError("Wifi is diconnected")
+        ntptime.settime()
         return
 
-    def get_time(self):
-        return utime.localtime()
-
+    def set_creation_time(self):
+        self.time_created =  utime.time()
+        return
+    
     def get_local_time_str(self):
         # TODO Daylight Saving Time
         dt = utime.localtime(utime.time() - TIME_ZONE_OFFSET)
-        ct = utime.localtime(utime.mktime(self.time_created) - TIME_ZONE_OFFSET)
+        ct = utime.localtime(self.time_created - TIME_ZONE_OFFSET)
         
         dt_str = "Created: " + str(ct[1]) + "-" + str(ct[2]) + " " + str(ct[3]) \
             + ":" + str(ct[4]) + " " + str(dt[0]) + "-" + str(dt[1]) + "-" \
