a776dd78f2d42d8e451f995c3a250267de532d0a
Joshua Fung <jfung@westhillinnvation.com>
2019-07-24 11:07:53 -0400

    renamed laser on off

--- gui_ctrl.py
+++ gui_ctrl.py
@@ -13,41 +13,46 @@ DISP_BUF_SIZE = const(9600)
 
 class LaserGui:
     def __init__(self):
-        # Initialize TFT Feather Wing Display and Touch Screen
+        # LVGL
         lv.init()
+        lv.task_core_init()
         
+        # MCU Control
         self._laser_mcu = laser_mcu.LaserMCU()
 
+        # TFT and TS driver
         self._tft = tftwing.TFTFeatherWing()
         self._tft.init()
-        
+
+        # TH sensor
+        self._th_ctrl = th_ctrl.THCtrl()
+
+        # Laser Measuring Control
+        self._laser = laser_ctrl.LaserCtrl()
+        self._laser.off()
+
+        # Load Time
         if self._laser_mcu.is_connected():
             self._laser_mcu.set_time_ntp()
         else:
             self._laser_mcu.load_time()
-            pass
-        
+
         self._laser_mcu.set_creation_time()
-            
-        self._th_ctrl = th_ctrl.THCtrl()
 
-        self._laser = laser_ctrl.LaserCtrl()
-        self._laser.off()
-
-        lv.task_core_init()
-        print("task1")
+        
+        # Task to update header, time and th value
         self._task_update_header = lv.task_create_basic()
         lv.task_set_cb(self._task_update_header, self.update_header)
         lv.task_set_period(self._task_update_header, 500)
         lv.task_set_prio(self._task_update_header, lv.TASK_PRIO.MID)
-        print("task2")
-        
+
+        # Task to save time to flash
         self._task_save_time = lv.task_create_basic()
         lv.task_set_cb(self._task_save_time, self._save_time)
         lv.task_set_period(self._task_save_time, 60000)
         lv.task_set_prio(self._task_save_time, lv.TASK_PRIO.MID)
         
-        print("buffer")
+        # Register display buffer, driver and input device driver
         self._disp_buf = lv.disp_buf_t()
         self._buf_1 = bytearray(DISP_BUF_SIZE)
         self._buf_2 = bytearray(DISP_BUF_SIZE)
@@ -59,22 +64,21 @@ class LaserGui:
 
         self._register_disp_drv()
         self._register_indev_drv()
-        print("load screen")
+
+        # Create screen
         self._load_screen()
 
-        print("task ready")
-        lv.task_ready(self._task_update_header)
-        
+        # Make task to run if not yet
+        lv.task_ready(self._task_update_header)        
         lv.task_ready(self._task_save_time)
-        
+        return
 
     def _save_time(self, data):
-        print("save Time")
         self._laser_mcu.save_time()
+        return
         
     def _register_disp_drv(self):
         # Init buffer
-        # TODO don't use len()
         lv.disp_buf_init(self._disp_buf, self._buf_1, self._buf_2, DISP_BUF_SIZE//4)
 
         # Register display driver
@@ -99,6 +103,7 @@ class LaserGui:
         th=lv.theme_night_init(210, lv.font_roboto_16)
         lv.theme_set_current(th)
         self._scr = lv.obj()
+        return
 
         # Add header
         self._header = lv.cont(self._scr)
@@ -116,12 +121,9 @@ class LaserGui:
         
         lv.scr_load(self._scr)
         return
-    def _set_datetime_gui(self):
-        self._datetime_gui =lv.obj()
-        self._cal = lv.calendar(self._datetime_gui)
-        lv.scr_load(self._datetime_gui)
         
     def call_task_handler(self):
+        print("LaserGui call_task_handler")
         lv.task_handler()
 
     def update_screen(self):
