81813b8241fa2c34d4d4315e1fc4e3cb3534e4eb
Joshua Fung <jfung@westhillinnvation.com>
2019-07-24 11:17:43 -0400

    renamed some function

--- gui_ctrl.py
+++ gui_ctrl.py
@@ -40,15 +40,6 @@ class LaserGui:
         self._laser_mcu.set_creation_time()
         
         # Register display buffer, driver and input device driver
-        self._disp_buf = lv.disp_buf_t()
-        self._buf_1 = bytearray(DISP_BUF_SIZE)
-        self._buf_2 = bytearray(DISP_BUF_SIZE)
-        self._disp_drv = lv.disp_drv_t()
-        self._disp = None
-        
-        self._indev_drv = lv.indev_drv_t()
-        self._indev = None
-
         self._register_disp_drv()
         self._register_indev_drv()
 
@@ -57,13 +48,13 @@ class LaserGui:
 
         # Task to update header, time and th value
         self._task_update_header = lv.task_create_basic()
-        lv.task_set_cb(self._task_update_header, self._update_header)
+        lv.task_set_cb(self._task_update_header, self._update_header_cb)
         lv.task_set_period(self._task_update_header, 500)
         lv.task_set_prio(self._task_update_header, lv.TASK_PRIO.MID)
 
         # Task to save time to flash
         self._task_save_time = lv.task_create_basic()
-        lv.task_set_cb(self._task_save_time, self._save_time)
+        lv.task_set_cb(self._task_save_time, self._save_time_cb)
         lv.task_set_period(self._task_save_time, 60000)
         lv.task_set_prio(self._task_save_time, lv.TASK_PRIO.MID)
 
@@ -71,13 +62,13 @@ class LaserGui:
         lv.task_ready(self._task_update_header)        
         lv.task_ready(self._task_save_time)
         return
-
-    def _save_time(self, data):
-        self._laser_mcu.save_time()
-        return
         
     def _register_disp_drv(self):
         # Init buffer
+        self._disp_buf = lv.disp_buf_t()
+        self._buf_1 = bytearray(DISP_BUF_SIZE)
+        self._buf_2 = bytearray(DISP_BUF_SIZE)
+        self._disp_drv = lv.disp_drv_t()
         lv.disp_buf_init(self._disp_buf, self._buf_1, self._buf_2, DISP_BUF_SIZE//4)
 
         # Register display driver
@@ -91,6 +82,7 @@ class LaserGui:
 
     def _register_indev_drv(self):
         # Register touch screen driver
+        self._indev_drv = lv.indev_drv_t()
         lv.indev_drv_init(self._indev_drv)
         self._indev_drv.type = lv.INDEV_TYPE.POINTER
         self._indev_drv.read_cb = self._tft.read
@@ -125,7 +117,7 @@ class LaserGui:
         lv.task_handler()
         return
 
-    def update_screen(self):
+    def _update_screen(self):
         if self._laser_mcu.is_connected():
             self._sym.set_text(self._laser_mcu.get_local_time_str() + " " + lv.SYMBOL.WIFI)
         else:
@@ -137,8 +129,11 @@ class LaserGui:
         gc.collect()
         return
 
-    def _update_header(self, data):
-        #print("Update Header")
-        self.update_screen()
+    def _update_header_cb(self, data):
+        self._update_screen()
+        return
+
+    def _save_time_cb(self, data):
+        self._laser_mcu.save_time()
         return
         
