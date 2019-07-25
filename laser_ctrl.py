8b73ccfdae8b23563ef1d494934fb1d5b333eeb3
Joshua Fung <jfung@westhillinnvation.com>
2019-07-25 19:25:16 -0400

    remove timeing log

--- laser_ctrl.py
+++ laser_ctrl.py
@@ -11,10 +11,9 @@ class LaserCtrl:
         self._laser.init(baudrate=38400)
         self._amp_stack = ((00,01),(02,03))
         self._read_buf = bytearray("0"*36)
-        self._pv = [0.0] * 4
+        self._pvs = [0.0] * 4
         self._laser_on = True
-        self.get_pvs()
-        print("init laser ctrl")
+        self.get_phrase_pvs()
 
     def reset_all(self):
         self.write_all("005","0")
@@ -24,7 +23,7 @@ class LaserCtrl:
 
     def get_values_str(self):
         pv_str = ""
-        for pv in self._pv:
+        for pv in self._pvs:
             pv_str += ("% 07.3f " % pv)
         return pv_str
 
@@ -34,59 +33,27 @@ class LaserCtrl:
             utime.sleep_us(1)
         self._laser.readinto(self._read_buf)
         for amp in range(0,4):
-            self._pv[amp] = float(self._read_buf[amp*8+3:amp*8+10])
-    
-    def get_pvs(self):
-        self._laser.write("M0\r\n")
-        while not self._laser.any():
-            utime.sleep_us(1)
-        self._laser.readinto(self._read_buf)
+            self._pvs[amp] = float(self._read_buf[amp*8+3:amp*8+10])
 
     def write_all(self, cmd, data):
-        start = utime.ticks_us()
         self._laser.write("AW,%s,%s\r\n" % (cmd, data))
-        print(utime.ticks_diff(utime.ticks_us(), start))
-        start = utime.ticks_us()
         while not self._laser.any():
             utime.sleep_us(1)
-        print(utime.ticks_diff(utime.ticks_us(), start))
-        start = utime.ticks_us()
-        print(self._laser.readline())
-        print(utime.ticks_diff(utime.ticks_us(), start))
+        self._laser.readline()
 
     def read_all(self, cmd):
         for laser_pair in self._amp_stack:
             for amp in laser_pair:
-                start = utime.ticks_us()
                 self._laser.write("SR,%02d,%s\r\n" % (amp, cmd))
-                print(utime.ticks_diff(utime.ticks_us(), start))
-                start = utime.ticks_us()
                 while not self._laser.any():
                     utime.sleep_us(1)
-                print(utime.ticks_diff(utime.ticks_us(), start))
-                start = utime.ticks_us()
-                print(self._laser.readline())
-                print(utime.ticks_diff(utime.ticks_us(), start))
+                self._laser.readline()
 
     def write_amp(self, amp, cmd, data):
-        start = utime.ticks_us()
         self._laser.write("SW,%02d,%s,%s\r\n" % (amp, cmd, data))
-        print(utime.ticks_diff(utime.ticks_us(), start))
-        start = utime.ticks_us()
         while not self._laser.any():
             utime.sleep_us(1)
-        print(utime.ticks_diff(utime.ticks_us(), start))
-        start = utime.ticks_us()
-        print(self._laser.readline())
-        print(utime.ticks_diff(utime.ticks_us(), start))
-
-    def toggle(self):
-        if self._laser_on:
-            self.off()
-            self._laser_on = False
-        else:
-            self.on()
-            self._laser_on = True
+        self._laser.readline()
 
     def off(self):
         self.write_all("100", "1")
