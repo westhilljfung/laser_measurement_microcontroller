1c040f13b323d81c68f67c8c0ab9858625a61eea
Joshua Fung <jfung@westhillinnvation.com>
2019-07-29 11:29:35 -0400

    @timed_function

--- gui_ctrl.py
+++ gui_ctrl.py
@@ -11,6 +11,16 @@ import laser_ctrl
 
 DISP_BUF_SIZE = const(9600)
 
+def timed_function(f, *args, **kwargs):
+    myname = str(f).split(' ')[1]
+    def new_func(*args, **kwargs):
+        t = utime.ticks_us()
+        result = f(*args, **kwargs)
+        delta = utime.ticks_diff(utime.ticks_us(), t)
+        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
+        return result
+    return new_func
+
 class LaserGui:
     def __init__(self):
         # LVGL
@@ -181,12 +191,4 @@ class LaserGui:
         self._laser_mcu.save_time()
         return
 
-def timed_function(f, *args, **kwargs):
-    myname = str(f).split(' ')[1]
-    def new_func(*args, **kwargs):
-        t = utime.ticks_us()
-        result = f(*args, **kwargs)
-        delta = utime.ticks_diff(utime.ticks_us(), t)
-        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
-        return result
-    return new_func
+
