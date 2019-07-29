d1f5085b14b9ab73084b839dc7592e00a6260da9
Joshua Fung <jfung@westhillinnvation.com>
2019-07-29 11:28:15 -0400

    @timed_function

--- gui_ctrl.py
+++ gui_ctrl.py
@@ -180,3 +180,13 @@ class LaserGui:
     def _save_time_cb(self, data):
         self._laser_mcu.save_time()
         return
+
+def timed_function(f, *args, **kwargs):
+    myname = str(f).split(' ')[1]
+    def new_func(*args, **kwargs):
+        t = utime.ticks_us()
+        result = f(*args, **kwargs)
+        delta = utime.ticks_diff(utime.ticks_us(), t)
+        print('Function {} Time = {:6.3f}ms'.format(myname, delta/1000))
+        return result
+    return new_func
